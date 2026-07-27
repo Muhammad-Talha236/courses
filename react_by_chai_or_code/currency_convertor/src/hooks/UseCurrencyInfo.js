3c0ea89fd4c9bf1da326a230574940126068f3e1
Muhammad-Talha236 <mtalha.mt236@gmail.com>
2026-07-27 02:07:17 -0700

    Add router notes

---
+++ react_by_chai_or_code/currency_convertor/src/hooks/UseCurrencyInfo.js
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+
+function useCurrencyInfo(initial) {
+    const [data, setData] = useState({});
+
+    useEffect(() => {
+        fetch(`https://latest.currency-api.pages.dev/v1/currencies/${currency}.json`)
+            .then(res => res.json())
+            .then(data => setData(data[currency]))
+    }, [currency]);
+
+    console.log(data);
+    return data;
+}
+
+export default useCurrencyInfo;
